11bd2afbf24fc6b9547737fadd57636d0b534f87
thesumyatmaw <thesumyatmaw@gmail.com>
2022-04-25 19:54:24 +0200

    format the code

--- cypress/integration/searchProduct.feature.js
+++ cypress/integration/searchProduct.feature.js
@@ -1,40 +1,28 @@
-/// <reference types="cypress"/>
-
 describe ('Buy Product Test Suite', ()=>{
-
-    describe('Given access the test link',()=>{
-        before(() =>{
+	describe('Given access the test link',()=>{
+		before(() => {
 			cy.visit('/')
 		})
-       
-        it('should display Sign in Link', () => {
-			cy.get('.login')
-           
+			
+		it('should display Sign in Link', () => {
+			cy.get('.login')				
 		})
 
-        describe('When a search product in the search box',()=>{
-            it('should display relevant product & click on Add to cart  ', () => {
-                cy.get('.cat-title')
-                cy.get('[id="searchbox"]').type('Printed Chiffon Dress ').wait(5000)
-                cy.get('.ac_results ul>li',{ timeout: 10000 }).eq(0).click()
-                
-            
-            })
+		describe('When a search product in the search box',()=> {
+			it('should display relevant product & click on Add to cart  ', () => {
+				cy.get('.cat-title')
+				cy.get('[id="searchbox"]').type('Printed Chiffon Dress ').wait(5000)
+				cy.get('.ac_results ul>li',{ timeout: 10000 }).eq(0).click()
+			})
+		})
 
-        })
-
-        describe(' When click on product category ',()=>{
-
-            it('should display product detail ', () => {
-                cy.get('.cat-title')
-                cy.get('ul.sf-menu').children()
-                cy.get('ul.sf-menu').children().eq(1).click()
-                
-                cy.get('.product_list').children()
-                          
-            })
-
-        })
-
-    })
+		describe(' When click on product category ',()=>{
+			it('should display product detail ', () => {
+				cy.get('.cat-title')
+				cy.get('ul.sf-menu').children()
+				cy.get('ul.sf-menu').children().eq(1).click()
+				cy.get('.product_list').children()					
+			})
+		})
+	})
 })
